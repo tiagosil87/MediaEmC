ab09e2bd71c8b5d400e35b232bde2ca589c2dc3b
Tiago Silva <tiagosil87@gmail.com>
2017-08-20 09:18:16 -0300

    Adicionando funcionamento básico

--- media.c
+++ media.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
+
+	float n1, n2;
+
+	printf("Calculadora de média\n\n");
+
+	printf("Digite o primeiro número: ");
+	scanf("%f", &n1);
+
+	printf("Digite o segundo número: ");
+	scanf("%f", &n2);
+
+	float media = (n1 + n2) / 2;
+
+	printf("\nA média é: %.2f\n", media);
+
 	return 0;
 }
